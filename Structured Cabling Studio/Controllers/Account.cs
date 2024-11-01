91ca89c8a08640b3b2f2a13d989c3964ad162188
Rhoxolan <pba17mail@gmail.com>
2024-11-01 23:17:48 +0200

    Added some project files

---
+++ Structured Cabling Studio/Controllers/Account.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StructuredCablingStudio.Controllers
+{
+	public class Account : Controller
+	{
+		[AllowAnonymous]
+		public IActionResult SignIn(string returnUrl)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[AllowAnonymous]
+		public IActionResult SignInWithGoogle(string returnUrl)
+		{
+			throw new NotImplementedException();
+		}
+
+		[AllowAnonymous]
+		public async Task<IActionResult> GoogleLoginCallback(string returnUrl)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Logout(string returnUrl)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Structured Cabling Studio/Controllers/Localization.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StructuredCablingStudio.Controllers
+{
+	public class Localization : Controller
+	{
+		public IActionResult SetLanguage(string culture, string returnUrl)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
