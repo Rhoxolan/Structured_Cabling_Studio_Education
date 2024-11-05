eafa52e045b66a1bbb33e91ef54925de6a7a18dd
Rhoxolan <pba17mail@gmail.com>
2024-11-05 23:22:38 +0200

    Added entities

---
+++ Structured Cabling Studio/Entities/CablingConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace StructuredCablingStudio.Entities
+{
+	public record CablingConfiguration : Models.CalculationModels.CablingConfiguration
+	{
+		public uint Id { get; set; }
+
+		public User User { get; set; } = default!;
+	}
+}
---
+++ Structured Cabling Studio/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StructuredCablingStudio.Entities
+{
+	public class User : IdentityUser
+	{
+		public ICollection<CablingConfiguration> Configurations { get; set; } = default!;
+	}
+}
